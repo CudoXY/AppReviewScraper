f925d4c9bc2d112780c5a6a61a6cd50aa6ad61b5
CudoXY <gelo103097@yahoo.com>
2019-06-16 21:17:49 +0800

    Changed implementation to XML RSS feed

--- appstore_scrape.py
+++ appstore_scrape.py
@@ -7,25 +7,25 @@ except ImportError:
 
 pipmain(['install', 'requests'])
 pipmain(['install', 'pandas'])
+pipmain(['install', 'feedparser'])
 
 from optparse import OptionParser
 from pathlib import Path
-from urllib.request import urlopen
-
-import json
+import feedparser
 import pandas as pd
 
 # Constants
-COLUMN_REVIEW_ID = 'id'
 COLUMN_TITLE = 'title'
+COLUMN_REVIEW_DATE = 'review_date'
 COLUMN_AUTHOR = 'author'
 COLUMN_AUTHOR_URL = 'author_url'
 COLUMN_VERSION = 'version'
 COLUMN_RATING = 'rating'
 COLUMN_REVIEW = 'review'
 COLUMN_VOTE_COUNT = 'vote_count'
+COLUMN_VOTE_SUM = 'vote_sum'
 
-URL_APPSTORE_REVIEWS = 'https://itunes.apple.com/rss/customerreviews/id=%s/page=%d/sortby=mostrecent/json'
+URL_APPSTORE_REVIEWS = 'https://itunes.apple.com/%s/rss/customerreviews/id=%s/page=%d/sortby=mostrecent/xml'
 
 
 def create_df():
@@ -35,19 +35,19 @@ def create_df():
     :return:
         The base Pandas DataFrame
     """
-    return pd.DataFrame(columns=[COLUMN_REVIEW_ID, COLUMN_TITLE, COLUMN_AUTHOR, COLUMN_AUTHOR_URL,
-                                 COLUMN_VERSION, COLUMN_RATING, COLUMN_REVIEW, COLUMN_VOTE_COUNT])
+    return pd.DataFrame(columns=[COLUMN_TITLE, COLUMN_REVIEW_DATE, COLUMN_AUTHOR, COLUMN_AUTHOR_URL,
+                                 COLUMN_VERSION, COLUMN_RATING, COLUMN_REVIEW, COLUMN_VOTE_COUNT, COLUMN_VOTE_SUM])
 
 
-
-
-def get_app_store_reviews(app_store_id, page_index=1):
+def get_app_store_reviews(app_store_id, country, page_index=1):
     """
-    Scrapes the App Store reviews based on the page index. As of this implementation, the App Store
-    only returns 30 reviews per page.
+    Scrapes the App Store reviews based on the page index.
 
-    :param play_store_id:
+    :param app_store_id:
         App Store App ID (starts at 1)
+
+    :param country:
+        The country where the reviews will be scraped from
 
     :param page_index:
         The page index
@@ -56,70 +56,50 @@ def get_app_store_reviews(app_store_id, page_index=1):
         The Pandas DataFrame object of the scraped values
     """
 
-    def parse_json(url):
+    def parse_xml(url):
         """
-        Parses the JSON object from the RSS URL
+        Parses the XML object from the RSS URL
 
         :param url:
             URL of the App Store RSS (in JSON format)
 
         :return:
-            The parsed JSON object
+            The parsed XML object
         """
-        response = urlopen(url)
-        response_data = str(response.read().decode("utf-8"))
-        return json.loads(response_data)
+        return feedparser.parse(url)
 
-    url = URL_APPSTORE_REVIEWS % (app_store_id, page_index)
-    data = parse_json(url).get('feed')
+    url = URL_APPSTORE_REVIEWS % (country, app_store_id, page_index)
+    data = parse_xml(url)
 
-    if data.get('entry') is None:
-        return
-
+    # Format the data into a table\
     df = create_df()
-    review_id_list = []
-    title_list = []
-    author_list = []
-    author_url_list = []
-    version_list = []
-    rating_list = []
-    review_list = []
-    vote_count_list = []
+    df[COLUMN_TITLE] = [entry['title'].replace('’', '\'') for entry in data.entries[1:]]
 
-    for entry in data.get('entry'):
-        if entry.get('im:name'):
-            continue
+    df[COLUMN_REVIEW_DATE] = [entry['updated'] for entry in data.entries[1:]]
+    df[COLUMN_REVIEW_DATE] = pd.to_datetime(df[COLUMN_REVIEW_DATE])
 
-        review_id_list.append(entry.get('id').get('label'))
-        title_list.append(entry.get('title').get('label').replace('"', '""'))
-        author_list.append(entry.get('author').get('name').get('label'))
-        author_url_list.append(entry.get('author').get('uri').get('label'))
-        version_list.append(entry.get('im:version').get('label'))
-        rating_list.append(entry.get('im:rating').get('label'))
-        review_list.append(entry.get('content').get('label').replace('"', '""'))
-        vote_count_list.append(entry.get('im:voteCount').get('label'))
-
-    # Format the data into a table
-    df[COLUMN_REVIEW_ID] = review_id_list
-    df[COLUMN_TITLE] = title_list
-    df[COLUMN_AUTHOR] = author_list
-    df[COLUMN_AUTHOR_URL] = author_url_list
-    df[COLUMN_VERSION] = version_list
-    df[COLUMN_RATING] = rating_list
-    df[COLUMN_REVIEW] = review_list
-    df[COLUMN_VOTE_COUNT] = vote_count_list
+    df[COLUMN_AUTHOR] = [entry['author_detail']['name'] for entry in data.entries[1:]]
+    df[COLUMN_AUTHOR_URL] = [entry['author_detail']['href'] for entry in data.entries[1:]]
+    df[COLUMN_VERSION] = [entry['im_version'] for entry in data.entries[1:]]
+    df[COLUMN_RATING] = [int(entry['im_rating']) for entry in data.entries[1:]]
+    df[COLUMN_REVIEW] = [entry['content'][0]['value'].replace('’', '\'') for entry in data.entries[1:]]
+    df[COLUMN_VOTE_COUNT] = [int(entry['im_votecount']) for entry in data.entries[1:]]
+    df[COLUMN_VOTE_SUM] = [int(entry['im_votesum']) for entry in data.entries[1:]]
 
     print("[*] Retrieved " + str(len(df)) + " reviews")
 
     return df
 
 
-def save_page_reviews(app_store_id, page_count, output_file_path):
+def save_page_reviews(app_store_id, country, page_count, output_file_path):
     """
     Scrapes and saves the App Store reviews based on the given the number of pages.
 
     :param app_store_id:
         App Store App ID
+
+    :param country:
+        The country where the reviews will be scraped from
 
     :param page_count:
         The number of page to be scraped
@@ -135,7 +115,7 @@ def save_page_reviews(app_store_id, page_count, output_file_path):
     output_file = Path(output_file_path)
     for i in range(0, int(page_count)):
 
-        df_review = get_app_store_reviews(app_store_id, i+1)
+        df_review = get_app_store_reviews(app_store_id, country, i + 1)
 
         if df_review is None or len(df_review) <= 0:
             continue
@@ -165,16 +145,20 @@ def main():
                       dest="output",
                       default="output.csv",
                       help="The output file where you want to dump results", )
+    parser.add_option("-c", "--country",
+                      action="store",
+                      dest="country",
+                      default="ph",
+                      help="The country code where the reviews will be scraped", )
 
     (options, args) = parser.parse_args()
 
     print("[*] Downloading the first " + str(options.pages) + " pages from: " + options.app_id)
 
-    save_page_reviews(options.app_id, options.pages, options.output)
+    save_page_reviews(options.app_id, options.country, options.pages, options.output)
 
     print("[*] Finished downloading to file " + options.output)
 
 
 if __name__ == '__main__':
     main()
-
